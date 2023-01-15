da7a8bd30095814ddcbc08cb606d5d1e5c3a979c
Farimah71 <farimah.fattahi2000@gmail.com>
2023-01-15 15:59:50 +0330

    Refactor sorting

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -46,15 +46,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: item, currentPage: 1 });
   };
 
-  handleSort = (path) => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path) {
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    } else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
 
@@ -97,6 +89,7 @@ class Movies extends Component {
             <div className="card" style={{ overflow: "hidden" }}>
               <MoviesTable
                 movies={movies}
+                sortColumn={sortColumn}
                 onLike={this.handleLike}
                 onDelete={this.handleDelete}
                 onSort={this.handleSort}
