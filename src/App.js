c31d062dfb6234edaecb5dc5b90bcf5c93624837
Farimah71 <farimah.fattahi2000@gmail.com>
2023-01-29 16:50:30 +0330

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Movies from "./components/Movies";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <main className="container">
+      <Navbar />
       <Movies />
     </main>
   );
